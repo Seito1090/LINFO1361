32c9033fb4d73b22c14f84404fb680f3a03da7e4
Seito the Wizard <krystian.targonski@student.uclouvain.be>
2024-03-10 22:14:35 +0100

    trying to push

---
+++ Assignment_2/LINFO1361-Shobu/template_alphabeta.py
@@ -0,0 +1,155 @@
+from agent import Agent
+
+class AlphaBetaAgent(Agent):
+    """An agent that uses the alpha-beta pruning algorithm to determine the best move.
+
+    This agent extends the base Agent class, providing an implementation of the play
+    method that utilizes the alpha-beta pruning technique to make decisions more efficiently.
+
+    Attributes:
+        max_depth (int): The maximum depth the search algorithm will explore.
+    """
+
+    def __init__(self, player, game, max_depth):
+        """Initializes an AlphaBetaAgent instance with a specified player, game, and maximum search depth.
+
+        Args:
+            player (int): The player ID this agent represents (0 or 1).
+            game (ShobuGame): The Shobu game instance the agent will play on.
+            max_depth (int): The maximum depth of the search tree.
+        """
+        super().__init__(player, game)
+        self.max_depth = max_depth
+
+    def play(self, state, remaining_time):
+        """Determines the best action by applying the alpha-beta pruning algorithm.
+
+        Overrides the play method in the base class.
+
+        Args:
+            state (ShobuState): The current state of the game.
+            remaining_time (float): The remaining time in seconds that the agent has to make a decision.
+
+        Returns:
+            ShobuAction: The action determined to be the best by the alpha-beta algorithm.
+        """
+        return self.alpha_beta_search(state)
+    
+    def is_cutoff(self, state, depth):
+        """Determines if the search should be cut off at the current depth.
+
+        Args:
+            state (ShobuState): The current state of the game.
+            depth (int): The current depth in the search tree.
+
+        Returns:
+            bool: True if the search should be cut off, False otherwise.
+        """
+        return self.max_depth == depth or self.game.is_terminal(state)
+    
+    def eval(self, state):
+        """Evaluates the given state and returns a score from the perspective of the agent's player.
+
+        Args:
+            state (ShobuState): The game state to evaluate.
+
+        Returns:
+            float: The evaluated score of the state.
+        """
+        # Just did what the pdf said
+        player_stones = state.board[self.player]
+        opponent_stones = state.board[1 - self.player]
+
+        player_min_stones = min(len(square) for square in player_stones)
+        opponent_min_stones = min(len(square) for square in opponent_stones)
+
+        score = (player_min_stones - opponent_min_stones) 
+
+        return score
+
+    def alpha_beta_search(self, state):
+        """Implements the alpha-beta pruning algorithm to find the best action.
+
+        Args:
+            state (ShobuState): The current game state.
+
+        Returns:
+            ShobuAction: The best action as determined by the alpha-beta algorithm.
+        """
+        _, action = self.max_value(state, -float("inf"), float("inf"), 0)
+        return action
+
+    def max_value(self, state, alpha, beta, depth):
+        """Computes the maximum achievable value for the current player at a given state using the alpha-beta pruning.
+
+        This method recursively explores all possible actions from the current state to find the one that maximizes
+        the player's score, pruning branches that cannot possibly affect the final decision.
+
+        Args:
+            state (ShobuState): The current state of the game.
+            alpha (float): The current alpha value, representing the minimum score that the maximizing player is assured of.
+            beta (float): The current beta value, representing the maximum score that the minimizing player is assured of.
+            depth (int): The current depth in the search tree.
+
+        Returns:
+            tuple: A tuple containing the best value achievable from this state and the action that leads to this value.
+                If the state is a terminal state or the depth limit is reached, the action will be None.
+        """
+
+        if self.is_cutoff(state, depth):
+            return self.eval(state), None
+
+        max_val = -float("inf") 
+        return_action = None
+
+        for action in self.game.actions(state):
+            test_state = self.game.result(state, action)
+            test_value, _ = self.min_value(test_state, alpha, beta, depth + 1)
+
+            if test_value > max_val:
+                max_val = test_value
+                return_action = action
+
+            alpha = max(alpha, max_val)
+            if max_val >= beta:
+                return max_val, return_action
+
+        return max_val, return_action
+
+
+    def min_value(self, state, alpha, beta, depth):
+        """Computes the minimum achievable value for the opposing player at a given state using the alpha-beta pruning.
+
+        Similar to max_value, this method recursively explores all possible actions from the current state to find
+        the one that minimizes the opponent's score, again using alpha-beta pruning to cut off branches that won't
+        affect the outcome.
+
+        Args:
+            state (ShobuState): The current state of the game.
+            alpha (float): The current alpha value, representing the minimum score that the maximizing player is assured of.
+            beta (float): The current beta value, representing the maximum score that the minimizing player is assured of.
+            depth (int): The current depth in the search tree.
+
+        Returns:
+            tuple: A tuple containing the best value achievable from this state for the opponent and the action that leads to this value.
+                If the state is a terminal state or the depth limit is reached, the action will be None.
+        """
+        if self.is_cutoff(state, depth):
+            return self.eval(state), None
+
+        min_val = float("inf") 
+        return_action = None
+
+        for action in self.game.actions(state):
+            test_state = self.game.result(state, action)
+            test_value, _ = self.max_value(test_state, alpha, beta, depth + 1)
+
+            if test_value < min_val:
+                min_val = test_value
+                return_action = action
+
+            beta = min(beta, min_val)
+            if min_val <= alpha:
+                return min_val, return_action
+
+        return min_val, return_action
---
+++ Assignment_2/LINFO1361-Shobu/template_uct.py
@@ -0,0 +1,192 @@
+from agent import Agent
+import random
+import math
+
+class Node:
+    """Node Class
+
+    A node in the MCTS tree.
+
+    Attributes:
+        parent (Node): The parent node of this node.
+        state (ShobuState): The game state represented by this node.
+        U (int): The total reward of the node. 
+        N (int): The number of times the node has been visited.
+        children (dict[Node, ShobuAction]): A dictionary mapping child nodes to their corresponding actions that lead to the state they represent.
+    """
+    def __init__(self, parent, state):
+        """Initializes a new Node object.
+
+        Args:
+            parent (Node): The parent node of this node.
+            state (ShobuState): The game state represented by this node.
+        """
+        self.parent = parent
+        self.state = state
+        self.U = 0
+        self.N = 0
+        self.children = {}
+
+class UCTAgent(Agent):
+    """An agent that uses the UCT algorithm to determine the best move.
+
+    This agent extends the base Agent class, providing an implementation of the play
+    method that utilizes UCT version of the MCTS algorithm.
+
+    Attributes:
+        player (int): The player id this agent represents.
+        game (ShobuGame): The game the agent is playing.
+        iteration (int): The number of simulations to perform in the UCT algorithm.
+    """
+
+    def __init__(self, player, game, iteration):
+        """Initializes a UCTAgent with a specified player, game, and number of iterations.
+
+        Args:
+            player (int): The player id this agent represents.
+            game (ShobuGame): The game the agent is playing.
+            iteration (int): The number of simulations to perform in the UCT algorithm.
+        """
+        super().__init__(player, game)
+        self.iteration = iteration
+
+    def play(self, state, remaining_time):
+        """Determines the next action to take in the given state.
+
+        Args:
+            state (ShobuState): The current state of the game.
+            remaining_time (float): The remaining time in seconds that the agent has to make a decision.
+
+        Returns:
+            ShobuAction: The chosen action.
+        """
+        return self.uct(state)
+
+    def uct(self, state):
+        """Executes the UCT algorithm to find the best action from the current state.
+
+        Args:
+            state (ShobuState): The current state of the game.
+
+        Returns:
+            ShobuAction: The action leading to the best-perceived outcome based on UCT algorithm.
+        """
+        root = Node(None, state)
+        root.children = { Node(root, self.game.result(root.state, action)): action for action in self.game.actions(root.state) }
+        for _ in range(self.iteration):
+            leaf = self.select(root)
+            child = self.expand(leaf)
+            result = self.simulate(child.state)
+            self.back_propagate(result, child)
+        max_state = max(root.children, key=lambda n: n.N)
+        return root.children.get(max_state)
+
+    def select(self, node):
+        """Selects a leaf node using the UCB1 formula to maximize exploration and exploitation.
+
+        The function recursively selects the children of the node that maximise the UCB1 score, exploring the most promising 
+        path in the game tree. It stops when a leaf is found and returns it. A leaf is either a node in a terminal state, 
+        or a node with a child for which no simulation has yet been performed.
+        
+        Args:
+            node (Node): The node to select from.
+
+        Returns:
+            Node: The selected leaf node.
+        """
+        while node.children:
+            unexplored_children = [child for child in node.children if child.N == 0]
+            if unexplored_children:
+                return node
+            ucb1_values = {child: self.UCB1(child) for child in node.children}
+            max_child = max(ucb1_values, key=ucb1_values.get)
+            node = max_child
+        return node
+    
+    def expand(self, node):
+        """Expands a node by adding a child node to the tree for an unexplored action.
+
+        The function returns one of the children of the node for which no simulation has yet been performed. 
+        In addition, the function must initialize all the children of that child node in the child's "children" dictionary. 
+        If the node is in a terminal state, the function returns itself, indicating that the node can no longer be expanded.
+
+        Args:
+            node (Node): The node to expand. This node represents the current state from which we want to explore possible actions.
+
+        Returns:
+            Node: The child node selected. If the node is at a terminal state, the node itself is returned.
+        """
+        if self.game.is_terminal(node.state) or not self.game.actions(node.state):
+            return node
+
+        unexplored_actions = [action for action in self.game.actions(node.state) if action not in node.children.values()]
+        if not unexplored_actions:
+            return node
+
+        selected_action = random.choice(unexplored_actions)
+
+        new_state = self.game.result(node.state, selected_action)
+        new_node = Node(node, new_state)
+
+        new_node.children = { Node(new_node, self.game.result(new_node.state, action)): action for action in self.game.actions(new_node.state) }
+
+        return new_node
+
+    def simulate(self, state):
+        """Simulates a random play-through from the given state to a terminal state.
+
+        Args:
+            state (ShobuState): The state to simulate from.
+
+        Returns:
+            float: The utility value of the resulting terminal state in the point of view of the opponent in the original state.
+        """
+        current_state = state
+        max_rounds = 500
+        rounds = 0
+
+        while not self.game.is_terminal(current_state) and rounds < max_rounds:
+            action = random.choice(self.game.actions(current_state))
+            current_state = self.game.result(current_state, action)
+            rounds += 1
+
+        return self.game.utility(current_state, (state.to_move + 1) % 2)
+
+    def back_propagate(self, result, node):
+        """Propagates the result of a simulation back up the tree, updating node statistics.
+
+        This method is responsible for updating the statistics for each node according to the result of the simulation. 
+        It recursively updates the U (utility) and N (number of visits) values for each node on the path from the given 
+        node to the root. The utility of a node is only updated if it is a node that must contain the win rate of the 
+        player who won the simulation, otherwise the utility is not modified.
+
+        Args:
+            result (float): The result of the simulation.
+            node (Node): The node to start backpropagation from.
+        """
+        node.N += 1  # Increment visit count
+
+        # Update win count if the result indicates a win for the player whose action led to this node
+        if result == 1:
+            node.U += 1
+
+        # If the node has a parent, recursively propagate the result to the parent node
+        if node.parent is not None:
+            self.back_propagate(result, node.parent)
+
+    def UCB1(self, node):
+        """Calculates the UCB1 value for a given node.
+
+        Args:
+            node (Node): The node to calculate the UCB1 value for. 
+
+        Returns:
+            float: The UCB1 value of the node. Returns infinity if the node has not been visited yet.
+        """
+
+        # formula for UCB1 : U(n) / N(n) + c * sqrt(logN(Parent(n)) / N(n))
+        c = math.sqrt(2)
+        if node.N == 0:
+            return float('inf')
+        else:
+            return node.U / node.N + c * math.sqrt(math.log(node.parent.N) / node.N)
