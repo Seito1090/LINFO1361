727fb3cd7badd92074af98eca02748281cc9c76d
vctrcc <victor.carballes@student.uclouvain.be>
2024-04-02 22:39:17 +0200

    Trying a structure

--- Assignment_2/LINFO1361-Shobu/template_contest.py
+++ Assignment_2/LINFO1361-Shobu/template_contest.py
@@ -131,3 +131,90 @@ class AI(Agent):
 
         return state.actions[0] # TODO: Replace this with your algorithm.
         ...
+
+
+
+
+
+    from time import time
+
+    MAX_BORING_ACTIONS = 20
+    MAX_DEPTH = 10
+
+    ID_TO_MOVE = 0
+    ID_UTILITY = 1
+    ID_BOARD = 2
+    ID_ACTIONS = 3
+    ID_COUNT_BORING_ACTIONS = 4
+
+    # IDEA : Use a tuple to describe a state, so that we can reference it with 'macros' instead of numbers
+    # Tuples are faster to create than objects, and are hashable -> Zobrist hashing/Transposition tables
+
+    def play_2(self, state, remaining_time):
+        offset_time = self.time() + remaining_time
+        player_id = state.to_move
+        is_winner = (state.utility == 1) & (player_id == 0) | (state.utility == -1) & (player_id == 1)
+
+        adapted_current_state = (state.to_move, state.utility, state.board, state.actions, state.count_boring_actions)
+
+        # Start the simulation
+        return self.simulate(adapted_current_state, player_id, is_winner, offset_time, self.MAX_DEPTH)
+    
+    def simulate(self, state, player_id, is_winner, offset_time, height):
+        # If we are too deep in the simulation, return None
+        if height <= 0: return None
+        # If the action leads to a stalemate or is boring, stop the simulation
+        if state[self.ID_COUNT_BORING_ACTIONS] > self.MAX_BORING_ACTIONS: return None
+        # Scoring board for actions
+        scores = np.zeros(len(state[self.ID_ACTIONS]))
+        # Get all actions possible
+        for id, action in enumerate(state[self.ID_ACTIONS]):
+            # If the time is up, return the best action
+            if self.time() > offset_time: return None
+            # Get the next state
+            next_state = self.generate_next_state(state, action, self.change_player(player_id))
+            # simulate the next state
+            next_action = self.simulate(next_state, self.change_player(player_id), is_winner, offset_time, height - 1)
+            # Score how good the action is
+            scores[id] = self.score_player(state, action, next_state, player_id)
+        
+        # Return the best action
+        return state.actions[np.argmax(scores)] # ?????
+        
+    def change_player(self, player_id):
+        return 1 - player_id
+    
+    def generate_next_state(self, state, action, player_id): # PRUNING HERE ?
+        new_board = state[self.ID_BOARD].copy()
+        # Apply the action
+        new_board[action[0]] = new_board[action[0]].replace(action[1], '')
+        new_board[action[2]] = new_board[action[2]] + action[1]
+        # Update the actions
+        new_actions = self.game.actions(new_board) # ?????????????????
+        # New Utility
+        new_utility = self.game.utility(new_board, player_id) # ?????????????????
+        # Update the count of boring actions
+        count_boring_actions = state[self.ID_COUNT_BORING_ACTIONS] + 1 if len(new_actions) == 1 else 0
+        # Return the new state
+        return (player_id, new_utility, new_board, new_actions, count_boring_actions)
+    
+    def score_player(self, state, action, next_state, player_id): # EVAL HERE ?
+        min_stones_white = 4
+        min_stones_black = 4
+
+        # Just did what the pdf said
+        for singleBoard in range(4):
+            white_stones, black_stones = next_state[self.ID_BOARD][singleBoard]
+            if len(white_stones) < min_stones_white:
+                min_stones_white = len(white_stones)
+            if len(black_stones) < min_stones_black:
+                min_stones_black = len(black_stones)
+        
+        # To make the relative score on the player id (0 or 1)
+        if player_id == 0:
+            score = (min_stones_white - min_stones_black) / 1.0
+        else:
+            score = (min_stones_black - min_stones_white) / 1.0
+        
+        return score
+    
